da62a19b3f1c16e3a588b865c3efa6b304f38671
Imam Mubin <imammubin@users.noreply.github.com>
2015-03-25 21:13:09 +0700

    Create header-megamenu.php

---
+++ header-megamenu.php
@@ -0,0 +1,341 @@
+		<header>
+			<nav class="navbar">
+				<div class="container">
+ 					<div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".menu-above">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+			  	<a class="navbar-brand" href="#">imammubin</a>
+
+					</div>
+            <div class="navbar-collapse menu-above collapse" >
+					
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#" class="uppercase">Home</a></li>
+					<li><a href="#" class="uppercase">about</a></li>
+					<li><a href="#" class="uppercase">portfolio</a></li> 
+					<li><a href="#" class="uppercase">contact</a></li> 
+ 
+				</ul>
+				
+				
+					
+                <ul class="nav navbar-nav navbar-right">
+					<li class="hidden-xs">
+ 						<ul class="nav navbar-nav">
+
+ 					<!-- mega-dropdown -->
+					<li class="dropdown">
+						<a href="http://imammubin.com/feed/" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-search"></span></a>
+						<div class="visible-lg dropdown-menu search">
+ 							<form role="search">
+							<input type="text"  data-width="80px" data-width-expanded="170px" class="form-control" name="q" placeholder="search, enter here" autocomplete="off">
+  						</div>
+					</li>
+					<!-- search LG -->	
+					<style>
+
+					</style>
+	
+							<li><a href="https://twitter.com/imammubin" class="fademe" target="_blank"><span class="fa fa-twitter"></span></a></li>
+							<li><a href="https://www.facebook.com/imammubinDotCom" class="fademe" target="_blank"><span class="fa fa-facebook"></span></a></li>
+							<li><a href="https://plus.google.com/116588403196434785856" class="fademe" target="_blank"><span class="fa fa-google-plus"></span></a></li>
+							<li><a href="http://imammubin.com/feed/" class="fademe" target="_blank"><span class="fa fa-rss"></span></a></li>
+ 						</ul>                                    
+ 					
+					</li>
+
+					<li class="visible-xs">
+ 						<ul class="nav navbar-nav">
+						
+						
+						<li class="col-xs-12">
+						<form class="navbar-form" role="search">
+						<div class="input-group">
+						<input type="text"  data-width="80px" data-width-expanded="170px" class="form-control" name="q" placeholder="Search here" autocomplete="off">
+						<span class="input-group-btn"><button class="btn" type="submit"><i class="glyphicon glyphicon-search"></i>&nbsp;</button></span>
+						
+						</div>
+						</form>						
+						</li>
+						
+						<li class="col-xs-3"><a href="https://twitter.com/imammubin" class="fademe" target="_blank"><span class="fa fa-twitter"></span></a></li>
+						<li class="col-xs-3"><a href="https://www.facebook.com/imammubinDotCom" class="fademe" target="_blank"><span class="fa fa-facebook"></span></a></li>
+						<li class="col-xs-3"><a href="https://plus.google.com/116588403196434785856" class="fademe" target="_blank"><span class="fa fa-google-plus"></span></a></li>
+						<li class="col-xs-3"><a href="http://imammubin.com/feed/" class="fademe" target="_blank"><span class="fa fa-rss"></span></a></li>
+						</ul>                                    
+ 					
+					</li>
+										
+					<!-- mega-dropdown -->
+					<li class="dropdown mega-dropdown">
+						<a href="#" data-toggle="dropdown" class="uppercase dropdown-toggle">blog<span class="glyphicon glyphicon-chevron-down pull-right"></span></a>
+					
+					<ul class="dropdown-menu mega-dropdown-menu row">
+					<li class="col-sm-3">
+						<ul>
+							<li class="dropdown-header">New in Stores</li>                            
+							<div id="myCarousel" class="carousel slide" data-ride="carousel">
+							  <div class="carousel-inner">
+								<div class="item active">
+									<a href="#"><img src="//placehold.it/254x150/3498db/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 1"></a>
+									<h4><small>Summer dress floral prints</small></h4>                                        
+									<button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>       
+								</div><!-- End Item -->
+								<div class="item">
+									<a href="#"><img src="//placehold.it/254x150/ef5e55/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 2"></a>
+									<h4><small>Gold sandals with shiny touch</small></h4>                                        
+									<button class="btn btn-primary" type="button">9,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>        
+								</div><!-- End Item -->
+								<div class="item">
+									<a href="#"><img src="//placehold.it/254x150/2ecc71/f5f5f5/&text=New+Collection" class="img-responsive" alt="product 3"></a>
+									<h4><small>Denin jacket stamped</small></h4>                                        
+									<button class="btn btn-primary" type="button">49,99 €</button> <button href="#" class="btn btn-default" type="button"><span class="glyphicon glyphicon-heart"></span> Add to Wishlist</button>      
+								</div><!-- End Item -->                                
+							  </div><!-- End Carousel Inner -->
+							</div><!-- /.carousel -->
+							<li class="divider"></li>
+							<li><a href="#">View all Collection <span class="glyphicon glyphicon-chevron-right pull-right"></span></a></li>
+						</ul>
+					</li>
+					<li class="col-sm-3">
+						<ul>
+							<li class="dropdown-header">Dresses</li>
+							<li><a href="#">Unique Features</a></li>
+							<li><a href="#">Image Responsive</a></li>
+							<li><a href="#">Auto Carousel</a></li>
+							<li><a href="#">Newsletter Form</a></li>
+							<li><a href="#">Four columns</a></li>
+							<li class="divider"></li>
+							<li class="dropdown-header">Tops</li>
+							<li><a href="#">Good Typography</a></li>
+						</ul>
+					</li>
+					<li class="col-sm-3">
+						<ul>
+							<li class="dropdown-header">Jackets</li>
+							<li><a href="#">Easy to customize</a></li>
+							<li><a href="#">Glyphicons</a></li>
+							<li><a href="#">Pull Right Elements</a></li>
+							<li class="divider"></li>
+							<li class="dropdown-header">Pants</li>
+							<li><a href="#">Coloured Headers</a></li>
+							<li><a href="#">Primary Buttons & Default</a></li>
+							<li><a href="#">Calls to action</a></li>
+						</ul>
+					</li>
+					<li class="col-sm-3">
+						<ul>
+							<li class="dropdown-header">Accessories</li>
+							<li><a href="#">Default Navbar</a></li>
+							<li><a href="#">Lovely Fonts</a></li>
+							<li><a href="#">Responsive Dropdown </a></li>							
+							<li class="divider"></li>
+							<li class="dropdown-header">Newsletter</li>
+							<form class="form" role="form">
+							  <div class="form-group">
+								<label class="sr-only" for="email">Email address</label>
+								<input type="email" class="form-control" id="email" placeholder="Enter email">                                                              
+							  </div>
+							  <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+							</form>                                                       
+						</ul>
+					</li>
+					<!-- mega-dropdown -->
+
+				</ul>
+
+                    </li>
+					
+					
+				</ul>
+				</div>
+ 			</nav>
+		</header>
+ 
+		
+		<style>
+			
+			/* NAVBAR */
+			.uppercase{ text-transform:uppercase;}
+			nav .container{ position:relative;}
+			.navbar { 
+				background: #3686be; background:rgba(1,1,1,.1); padding:0px;  margin-bottom:1px; position:fixed; left:0; right:0; top:0; -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+box-shadow: 0 6px 12px rgba(0,0,0,.175); z-index: 1;}
+
+				/* logo */
+				.navbar-header{ display:table; }
+				.navbar-header a{ vertical-align:bottom; text-shadow:none; color:#f9f9f9; }
+				.navbar-header a.navbar-brand { height: 70px; background:none;  line-height:38px;  background:rgba(1,1,1,.0251);}
+				.navbar-header a.navbar-brand:hover {   background:rgba(1,1,1,.1); }
+
+				/* NAVIGASI BAR TOGLE*/
+				.navbar-toggle{ margin-top:17px; }
+				.navbar-toggle .icon-bar { background: #f9f9f9; }
+				
+				@media (max-width:768px){
+					.navbar-collapse {
+					
+						position: absolute;
+						left: 0px;
+						right: 0px;
+						top:50px;
+						z-index: 2;
+						background:#3686be;					
+					
+					}
+					.navbar-header a.navbar-brand { height: 50px; }
+					.navbar-toggle { margin-top: 8px; }
+					.navbar-brand { padding: 6px 15px; }
+				}
+				
+				/* search form */
+ 				
+				/* SEARCH LG */
+				.dropdown.open .visible-lg{ display:block !Important; padding: 7px 0px;}
+				.dropdown .visible-lg form{  background:rgba(1,1,1,.1); margin-top:0px; }
+				.dropdown.open .visible-lg form{ margin-top:10px; }
+				.dropdown-menu.search {
+				 top: 0;
+				margin-right: 37px;
+				background: none;
+				border: 0px;
+				padding: 17px 0px;
+				box-shadow: none;
+				width:350px;
+				}
+				.dropdown-menu.search  input{ 
+					color:#333; border-radius:2px; -webkit-border-radius:2px; -moz-border-radius:2px;
+					-webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175); box-shadow: 0 6px 12px rgba(0,0,0,.175); -moz-box-shadow: 0 6px 12px rgba(0,0,0,.175);					
+				}
+				
+				
+				
+				
+				
+				.nav>li>a:focus, .nav>li>a:hover { text-decoration: none; background:rgba(1,1,1,.1);  } /* HOVER color */
+				.nav .open>a, .nav .open>a:focus, .nav .open>a:hover { background:none; border-color: #337ab7; }
+				.dropdown a:hover  .caret{ color:gray;   }
+				.dropdown-menu>li>a:focus, .dropdown-menu>li>a:hover { background: #f1f1f1; background-image:none;}
+				.nav li a{ padding:25px 10px; color:#fff; text-shadow:none;  }		
+				@media(max-width:768px){ 
+					.navbar .navbar-header { display:block; }
+					.nav li a{ padding:5px 10px; }		
+					.navbar .visible-xs .col-xs-3 a{ text-align:center;   margin: 10px 0px;}
+					.navbar { background:#3686be;  position: static; }
+
+				}
+				
+				
+					
+			@media   (min-width:768px) and (max-width:992px){
+			nav .container{ position:relative; width:100%; max-width:99%;  }
+			}
+ 
+			@media(max-width:768px){
+				.navbar-collapse.in {overflow-y: none !important; }
+				.container>.navbar-collapse{margin-right: 0; margin-left: 0 ; !important}
+ 				.navbar-nav {margin: 7px 0px;}
+				.container>.navbar-header {  margin-right: -15px;   margin-left: -15px; }
+			}
+			
+			
+			
+			
+			/* 
+				mega menu 
+			*/
+ 
+.navbar-nav .dropdown-menu.mega-dropdown-menu {
+	background:#f9f9f9;  margin-top:20px;
+}
+
+.mega-dropdown {
+position: static !important;
+margin-left: 20px;
+
+}
+.mega-dropdown  span.glyphicon.glyphicon-chevron-down.pull-right { margin-left: 11px; }
+.mega-dropdown  .glyphicon{ line-height:19px;}
+ .mega-dropdown li a{ padding:10px;}
+.mega-dropdown-menu {
+    padding: 20px 0px;
+    width: 100%;
+	background:#f9f9f9;
+}
+
+.mega-dropdown-menu > li > ul {
+  padding: 0;
+  margin: 0;
+}
+.mega-dropdown-menu > li > ul > li {
+  list-style: none;
+}
+.mega-dropdown-menu > li > ul > li > a {
+  display: block;
+  padding: 3px 20px;
+  clear: both;
+  font-weight: normal;
+  line-height: 1.428571429;
+  color: #999;
+  white-space: normal;
+}
+.mega-dropdown-menu > li ul > li > a:hover,
+.mega-dropdown-menu > li ul > li > a:focus {
+  text-decoration: none;
+  color: #444;
+  background-color: #f5f5f5;
+}
+.mega-dropdown-menu .dropdown-header {
+  color: #428bca;
+  font-size: 18px;
+  font-weight:bold;
+}
+.mega-dropdown-menu form {
+    margin:3px 20px;
+}
+.mega-dropdown-menu .form-group {
+
+    margin-bottom: 3px;
+	
+}	
+
+.mega-dropdown-menu:before {
+    content: "";
+    border-bottom: 15px solid #3686be;
+    border-right: 17px solid transparent;
+    border-left: 17px solid transparent;
+    position: absolute;
+    top: -15px;
+    right: 15px;
+    z-index: 10;
+}
+.mega-dropdown-menu:after {
+    content: "";
+    border-bottom: 17px solid #3686be;
+    border-right: 19px solid transparent;
+    border-left: 19px solid transparent;
+    position: absolute;
+    top: -17px;
+    right: 13px;
+    z-index: 8;
+}
+/*
+body::before {
+  content: "xs";
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 9999999;
+  background-color: #000;
+  color: #fff;
+}
+@media (min-width : 768px) { body::before { content: "sm"; }}
+@media (min-width : 992px) { body::before { content: "md"; }}
+@media (min-width : 1200px) { body::before { content: "lg"; }}
+*/
+	</style>		
+	
